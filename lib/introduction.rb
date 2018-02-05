c6750c5e85e058712390723c67bb2c6d67a83a21
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-02-05 00:07:13 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language(language)
+def introduction_with_language(name, language)
   puts "I am learning to program in #{language}"
 end
